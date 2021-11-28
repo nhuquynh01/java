3272f68fa00db20c3e326103d249f1a873c4f842
nhuquynh01 <11194437@st.neu.edu.vn>
2021-11-28 18:21:58 +0700

    master

--- script1.js
+++ script1.js
@@ -3,8 +3,7 @@ const modal = document.querySelector('.js-modal')
 const modalContainer = document.querySelector('.js-modal-container')
 const modalClose = document.querySelector('.js-modal-close')
 const booking = document.querySelector('.booking-now')
-const modalBook = document.querySelector('.js-modal-book')
-const modalBookClose = document.querySelector('.js-modal-book-close')
+
 
 
 function showbooking(){
@@ -14,26 +13,22 @@ function hidebooking(){
     modal.classList.remove('open')
 }
 
-function showSuccess(){
-    modal.classList.remove('open')
-    modalBook.classList.add('open1')
-}
-function hideSuccess(){
-    modalBook.classList.remove('open1')
-}
+
 
 for(const book of books) {
     book.addEventListener('click', showbooking)
 }
 
 modalClose.addEventListener('click', hidebooking)
-modalBookClose.addEventListener('click', hideSuccess)
+
 
 modal.addEventListener('click', hidebooking)
 
 modalContainer.addEventListener('click', function(event){
     event.stopPropagation()
 })
-booking.addEventListener('click', showSuccess)
+
+
+
 
 
